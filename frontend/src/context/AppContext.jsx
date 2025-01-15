3208447a2b4b76fbd6d9982d65620168b79b0aa0
BheemaReddy07 <bheemareddy29102003@gmail.com>
2025-01-15 08:28:55 +0530

    topdoctors and appointmentsmodification

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -7,8 +7,11 @@ export const AppContext = createContext()
 
 const AppContextProvider = (props) =>{
 
+
+    const currencySymbol = '₹'
+
     const value ={
-        doctors
+        doctors,currencySymbol
     }
 
     return (
